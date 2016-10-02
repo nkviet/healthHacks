01693b90faeb240ea1726a13d644b1743cc7c81e
Viet Nguyen <nkviet93@vt.edu>
2016-10-02 12:53:53 -0400

    Update healthhacks.ino
    adjusted to stop after step code runs and run again when the angle is in the parameters

--- healthhacks.ino
+++ healthhacks.ino
@@ -9,6 +9,7 @@ boolean reader = false;
 
 //stepper motor object
 Stepper sstepper(motorRev, 8, 10, 9, 11);
+//Stepper sstepper(motorRev, 8, 10, 9, 11);
 
 void setup()
 {
@@ -25,14 +26,18 @@ void loop()
   Serial.println(val);
   if(val>=60&&val<=150)
   {
-    Steps2Take  =  outputRev ;  // Rotate Clockwise 1 turn
-    sstepper.setSpeed(500);   
-    sstepper.step(Steps2Take);
+    while(val<60||val>150)
+    {
+      break;
+    }
+      Steps2Take  =  outputRev ;  // Rotate Clockwise 1 turn
+      sstepper.setSpeed(500);   
+      sstepper.step(Steps2Take);
     //delay(1000);
     
-    Steps2Take  =  - outputRev;  // Rotate counterclockwise 1 turn  
-    sstepper.setSpeed(500);
-    sstepper.step(Steps2Take);
+    //Steps2Take  =  - outputRev;  // Rotate counterclockwise 1 turn  
+    //sstepper.setSpeed(500);
+    //sstepper.step(Steps2Take);
     //delay(2000);
   }
 }
